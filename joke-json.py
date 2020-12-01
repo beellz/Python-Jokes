73c3b9eb73d70379965c9c1e56b93909e73fc519
beellz <belal20795@gmail.com>
2020-12-01 17:44:03 +0530

    changed the voice to female

--- joke-json.py
+++ joke-json.py
@@ -3,6 +3,16 @@ import json
 import pyttsx3
 
 
+engine = pyttsx3.init()
+
+rate = engine.getProperty('rate')   # getting details of current speaking rate
+print (rate)                        #printing current voice rate
+engine.setProperty('rate', 150)     # setting up new voice rate
+
+
+voices = engine.getProperty('voices') 
+voices = engine.setProperty('voice', voices[1].id)
+
 URL = "https://official-joke-api.appspot.com/random_ten"
 r = request.urlopen(URL)
 # print(r.getcode())
