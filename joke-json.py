2dbc564806f605b185e330079eec003c77da98f2
beellz <belal20795@gmail.com>
2020-12-01 17:27:10 +0530

    api call working and stagging right

---
+++ joke-json.py
@@ -0,0 +1,33 @@
+from urllib import request 
+import json
+
+
+URL = "https://official-joke-api.appspot.com/random_ten"
+r = request.urlopen(URL)
+# print(r.getcode())
+data = r.read()
+jsondata = json.loads(data)
+# print(jsondata)
+
+class Joke:
+    def __init__(self, setup, punchline) -> None:
+        self.setup = setup
+        self.punchline = punchline
+
+    def __str__(self) -> str:
+        return f"setup: {self.setup} punchline: {self.punchline}"
+
+jokes = []
+
+for j in jsondata:
+    setup = j["setup"]
+    punchline = j["punchline"] 
+    joke = Joke(setup, punchline)
+    jokes.append(joke)
+
+print(f" got {len(jokes)} jokes")
+
+for joke in jokes:
+    print(joke)
+
+# print(r.read())
